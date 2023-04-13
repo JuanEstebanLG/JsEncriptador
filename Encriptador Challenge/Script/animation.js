550f6a6b8630708d79bdada21882444de12cb2bd
JuanEstebanLG <56986294+JuanEstebanLG@users.noreply.github.com>
2023-04-13 15:03:28 -0500

    delete for move

--- Encriptador Challenge/Script/animation.js
+++
@@ -1,6 +0,0 @@
-const inputText = document.getElementById("input-text");
-const animatedText = document.getElementById("animated-text");
-
-inputText.addEventListener("input", () => {
-  animatedText.innerText = inputText.value;
-});
